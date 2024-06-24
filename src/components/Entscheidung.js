066fbd58b66c526f78309a16a58f16d900c5d8a2
Holm_Sven <svenholmstudium@gmail.com>
2024-06-24 23:11:29 +0200

    buttons

--- src/components/Entscheidung.js
+++ src/components/Entscheidung.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import styles from '../styles/Entscheidung.module.css'; 
 import InputPage from './InputPage';
-
+ 
 const Entscheidung = () => {
   const [activeImage, setActiveImage] = useState(null);
   const [showInputPage, setShowInputPage] = useState(false);
   const [gewuenscht, setGewuenscht] = useState(null);
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-
+ 
   const handleImageClick = (image) => {
     setActiveImage(activeImage === image ? null : image);
   };
-
+ 
   const handleWeiterClick = () => {
     if (activeImage === 'left') {
       setShowInputPage(true);
@@ -22,11 +22,11 @@ const Entscheidung = () => {
       setGewuenscht(1);
     }
   };
-
+ 
   if (showInputPage) {
     return <InputPage gewuenscht={gewuenscht} />;
   }
-
+ 
   if (isMobile) {
     return (
       <div className={styles.entscheidung}>
@@ -38,13 +38,14 @@ const Entscheidung = () => {
               <>
                 <div className={styles['text-box']} style={{ top: 'calc(100% + 10px)', left: '50%', transform: 'translateX(-50%)' }}>
                   <p>Eine optimale Ernährung für den Muskelaufbau erfordert ausreichend Protein zur Gewebereparatur und -entwicklung. Kohlenhydrate bieten Energie für das Training, während gesunde Fette den Hormonhaushalt unterstützen. Qualitativ hochwertige Kalorien sind entscheidend für das Muskelwachstum und die Regeneration nach dem Training. Essen von Nährstoffen aus Obst, Gemüse und Getreide trägt zur Energie bei.</p>
+                  <button className={styles['weiter-button']} onClick={handleWeiterClick}>Weiter</button>
                 </div>
               </>
             )}
           </div>
-
+ 
           {!activeImage && <p className={styles['zwischen-text']}>Wählen Sie Ihr Ziel</p>}
-
+ 
           <div className={`${styles['image-container']} ${activeImage === 'right' ? styles.active : ''}`} onClick={() => handleImageClick('right')}>
             <img src="./images/slim.jpeg" alt="Slim" className={styles.image} />
             <p className={styles.caption}>Abnehmen</p>
@@ -52,8 +53,8 @@ const Entscheidung = () => {
               <>
                 <div className={styles['text-box']} style={{ bottom: 'calc(100% + 10px)', left: '50%', transform: 'translateX(-50%)' }}>
                   <p>Eine ausgewogene Ernährung mit Kaloriendefizit hilft, Körperfett zu reduzieren und das Erscheinungsbild zu verbessern. Ausreichend Protein erhält Muskelmasse, während gesunde Fette und komplexe Kohlenhydrate den Stoffwechsel unterstützen. Nährstoffreiche Lebensmittel wie Obst, Gemüse und Vollkornprodukte fördern das Wohlbefinden und den Gewichtsverlust, indem sie langanhaltende Energie liefern.</p>
+                  <button className={styles['weiter-button']} onClick={handleWeiterClick}>Weiter</button>
                 </div>
-                <button className={styles['weiter-button']} onClick={handleWeiterClick}>Weiter</button>
               </>
             )}
           </div>
@@ -61,7 +62,7 @@ const Entscheidung = () => {
       </div>
     );
   }
-
+ 
   return (
     <div className={styles.entscheidung}>
       <div className={`${styles['entscheidung-container']} ${activeImage ? styles['blur-background'] : ''}`}>
@@ -72,13 +73,14 @@ const Entscheidung = () => {
             <>
               <div className={styles['text-box']}>
                 <p>Eine optimale Ernährung für den Muskelaufbau erfordert ausreichend Protein zur Gewebereparatur und -entwicklung. Kohlenhydrate bieten Energie für das Training, während gesunde Fette den Hormonhaushalt unterstützen. Qualitativ hochwertige Kalorien sind entscheidend für das Muskelwachstum und die Regeneration nach dem Training. Essen von Nährstoffen aus Obst, Gemüse und Getreide trägt zur Energie bei.</p>
+                <button className={styles['weiter-button']} onClick={handleWeiterClick}>Weiter</button>
               </div>
             </>
           )}
         </div>
-
+ 
         {!activeImage && <p className={styles['zwischen-text']}>Wählen Sie Ihr Ziel</p>}
-
+ 
         <div className={`${styles['image-container']} ${activeImage === 'right' ? `${styles.active} ${styles.right}` : ''}`} onClick={() => handleImageClick('right')}>
           <img src="./images/slim.jpeg" alt="Slim" className={styles.image} />
           <p className={styles.caption}>Abnehmen</p>
@@ -86,6 +88,7 @@ const Entscheidung = () => {
             <>
               <div className={styles['text-box']}>
                 <p>Eine ausgewogene Ernährung mit Kaloriendefizit hilft, Körperfett zu reduzieren und das Erscheinungsbild zu verbessern. Ausreichend Protein erhält Muskelmasse, während gesunde Fette und komplexe Kohlenhydrate den Stoffwechsel unterstützen. Nährstoffreiche Lebensmittel wie Obst, Gemüse und Vollkornprodukte fördern das Wohlbefinden und den Gewichtsverlust, indem sie langanhaltende Energie liefern.</p>
+                <button className={styles['weiter-button']} onClick={handleWeiterClick}>Weiter</button>
               </div>
             </>
           )}
@@ -94,5 +97,5 @@ const Entscheidung = () => {
     </div>
   );
 };
-
+ 
 export default Entscheidung;
