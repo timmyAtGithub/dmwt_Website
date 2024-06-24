c82148a71d4fa79820c1271463a80f18dbfd784a
TaxisTim-Luca <Tim-Luca.Taxis@Student.Reutlingen-University.DE>
2024-06-25 01:11:05 +0200

    Abgabe

--- src/components/Streak.js
+++ src/components/Streak.js
@@ -34,18 +34,14 @@ const Streak = ({ userId }) => {
           };
 
          
-          console.log(`Date: ${entry.date}`);
-          console.log(`Carbs: ${carbs} (Expected: ${maxCarbs ? maxCarbs / 4 * 0.9 : 'N/A'}-${maxCarbs ? maxCarbs / 4 * 1.1 : 'N/A'})`);
-          console.log(`Protein: ${protein} (Expected: ${maxProtein ? maxProtein / 4 * 0.9 : 'N/A'}-${maxProtein ? maxProtein / 4 * 1.1 : 'N/A'})`);
-          console.log(`Fat: ${fat} (Expected: ${maxFat ? maxFat / 8 * 0.9 : 'N/A'}-${maxFat ? maxFat / 8 * 1.1 : 'N/A'})`);
-          console.log(`Calories: ${calories} (Expected: ${maxCalories ? maxCalories * 0.9 : 'N/A'}-${maxCalories ? maxCalories * 1.1 : 'N/A'})`);
+
 
           const isCarbsGreen = inGreenZone(carbs, maxCarbs / 4);
           const isProteinGreen = inGreenZone(protein, maxProtein / 4);
           const isFatGreen = inGreenZone(fat, maxFat / 8);
           const isCaloriesGreen = inGreenZone(calories, maxCalories);
 
-          console.log(`isCarbsGreen: ${isCarbsGreen}, isProteinGreen: ${isProteinGreen}, isFatGreen: ${isFatGreen}, isCaloriesGreen: ${isCaloriesGreen}`);
+          
 
           if (isCarbsGreen && isProteinGreen && isFatGreen && isCaloriesGreen) {
             
